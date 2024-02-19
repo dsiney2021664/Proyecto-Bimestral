29dbdbf13f6f196d6ac479573031d3799a8c293f
dsiney2021664 <dsiney-2021664@kinal.edu.gt>
2024-02-18 21:39:36 -0600

    Se agrego la conexión del servidor y base de datos

---
+++ db/config.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_CNN, {});
+        console.log('Base de datos conectada exitosamente');
+    } catch (e) {
+        throw new Error('Error al hacer la conexion de la base de datos', e)
+    }
+}
+
+module.exports = { dbConnection }
